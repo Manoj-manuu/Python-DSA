cb782ca957cd3ffc628e0fbce407d697fc97ade7
manoj <manojchitrgar@gmail.com>
2026-07-11 00:46:14 +0530

    Two Sum

---
+++ My_Code/HashTables/Problems/two_sum.py
@@ -0,0 +1,13 @@
+def two_sum(nums,target):
+    seen={}
+    for i,num in enumerate(nums):
+        compound = target - num
+        if compound in seen:
+            return [seen[compound],i]
+        else:
+            seen[num] = i
+    return []
+
+print(two_sum([5, 1, 7, 2, 9, 3], 10))  
+print(two_sum([4, 2, 11, 7, 6, 3], 9))
+print ( two_sum([], 0) )
