1a3d6b6b4e96b11a311754f4c99872ed43dca9ca
manoj <manojchitrgar@gmail.com>
2026-07-17 23:50:34 +0530

    Added more test case

--- My_Code/HashTables/Set/longest_consecutive_sequence.py
+++ My_Code/HashTables/Set/longest_consecutive_sequence.py
@@ -3,7 +3,7 @@ def longest_consecutive_sequence(nums):
     longest_sequence = 0
     
     for num in nums:
-        if num - 1 not in my_set:
+        if num - 1 not in my_set: 
             current_num  =  num
             current_sequence = 1
             
@@ -16,3 +16,4 @@ def longest_consecutive_sequence(nums):
     return longest_sequence
         
 print(longest_consecutive_sequence([100, 4, 200, 1, 3, 2]) )
+print(longest_consecutive_sequence([10,20, 1, 3, 2]) )
